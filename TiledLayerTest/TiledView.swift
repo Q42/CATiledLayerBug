057523f4165d3c05f7c64e9b99cdf1712c73f719
Tom Lokhorst <tom@lokhorst.eu>
2016-10-07 22:49:38 +0200

    Include more logging

--- TiledLayerTest/TiledView.swift
+++ TiledLayerTest/TiledView.swift
@@ -41,10 +41,46 @@ class TiledView : UIView {
   override func draw(_ rect: CGRect) {
     guard let context = UIGraphicsGetCurrentContext() else { return }
 
+    let info = DebugInfo(context: context, rect: rect, tileSize: tiledLayer.tileSize)
+    print("BEGIN drawTile \(info.row),\(info.col) - \(tileColor)")
+
     tileColor.set()
     context.fill(rect)
+    drawDebugString(context: context, rect: rect, info: info)
 
     // Sleep to simulate "expensive" work on this render thread
     Thread.sleep(forTimeInterval: 0.1)
+
+    print("END   drawTile \(info.row),\(info.col) - \(tileColor)")
+  }
+
+
+  private func drawDebugString(context: CGContext, rect: CGRect, info: DebugInfo) {
+    let level = 1 / info.scale
+    let attributes: [String: AnyObject] = [
+      NSForegroundColorAttributeName: UIColor.white,
+      NSFontAttributeName: UIFont.systemFont(ofSize: level * 25)
+    ]
+
+    NSAttributedString(string: "\(info.row),\(info.col) / \(info.scale)", attributes: attributes)
+      .draw(in: rect.insetBy(dx: level * 10, dy: level * 10))
+  }
+}
+
+private struct DebugInfo {
+  let col: Int
+  let row: Int
+  let scale: CGFloat
+
+  init(context: CGContext, rect: CGRect, tileSize: CGSize) {
+    let scaleX = context.ctm.a
+    let scaleY = -context.ctm.d
+
+    let width = tileSize.width / scaleX
+    let height = tileSize.height / scaleY
+
+    col = Int(rect.minX / width)
+    row = Int(rect.minY / height)
+    scale = scaleX
   }
 }
